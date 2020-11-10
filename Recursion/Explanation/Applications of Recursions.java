fba3b4ec395ef90a3c424f53c15ea204d1f5364d
Kavyashree B S <kavyashreebgr1999@gmail.com>
2020-11-10 10:28:14 +0530

    Update Applications of Recursions.java

--- Recursion/Explanation/Applications of Recursions.java
+++ Recursion/Explanation/Applications of Recursions.java
@@ -1,3 +1,18 @@
 //Applications of Recursion
 
-which programs 
+//which programs which can be written using recursion can also be write using iteration
+
+// where we use recursion
+
+/*--Many algorithm techniques are based on recursion
+    ->Dynamic programming
+    -> Backtracking
+    ->Divide and conquer (binary search, quick sort and merge sort)
+  
+--Many problems inherently recursive
+    ->Towers of Hanoi
+    -> Dps based intervals (DFS of concepts and Inorder , preorder and postorder traversal of trees)
+*/
+1.Real time example of DFS is searching for a file in computer
+
+function call override - problem with recursion
