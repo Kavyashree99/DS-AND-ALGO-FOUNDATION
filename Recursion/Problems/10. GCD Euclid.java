51b039a979a86fc3eb077f79b93aa023d4ae684e
Kavyashree B S <kavyashreebgr1999@gmail.com>
2020-11-10 18:31:43 +0530

    Create 10. GCD Euclid.java

---
+++ Recursion/Problems/10. GCD Euclid.java
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            
+            System.out.println(new Solution().GCD(a, b));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // Complete the function
+    // a: first number
+    // b: second number
+    public static int GCD(int a, int b)
+    {
+        // find and return GCD of two numbers;
+    }
+}
