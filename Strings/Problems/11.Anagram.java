0a9a3c5c47f7c3324efd4899683c1ceee1b1875c
Kavyashree B S <kavyashreebgr1999@gmail.com>
2020-11-30 22:14:41 +0530

    Create 11.Anagram.java

---
+++ Strings/Problems/11.Anagram.java
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+	public static void main (String[] args)throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0)
+		{
+		    String str = br.readLine();
+		    String s1 = str.split(" ")[0];
+		    String s2 = str.split(" ")[1];
+		    
+		    Anagram obj = new Anagram();
+		    
+		    if(obj.isAnagram(s1,s2))
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		         System.out.println("NO");
+		    }
+		    
+		    
+		    
+		}
+	}
+}// } Driver Code Ends
+
+
+class Anagram{    
+    /*  Function to check if two strings are anagram
+    *   a, b: input string
+    */
+    public static boolean isAnagram(String s1,String s2)
+    {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] count = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i)]++;
+            count[s2.charAt(i)]--;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
